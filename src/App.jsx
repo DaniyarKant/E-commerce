807bff2c3e65d44b07c67e2a842ad0a80404a266
Daniyar <d.kanat1@mail.ru>
2024-03-26 21:41:04 -0300

    Settig up paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import AppLayout from "./ui/AppLayout";
+import HomePage from "./pages/HomePage";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            staleTime: 60 + 1000,
+        },
+    },
+});
 
 function App() {
     return (
         <QueryClientProvider client={queryClient}>
             <ReactQueryDevtools initialIsOpen={false} buttonPosition="bottom-left" />
-            <div>Hello world</div>
+            <BrowserRouter>
+                <Routes>
+                    <Route element={<AppLayout />}>
+                        <Route index element={<Navigate replace to="homepage" />} />
+                        <Route path="homepage" element={<HomePage />} />
+                    </Route>
+                </Routes>
+            </BrowserRouter>
         </QueryClientProvider>
     );
 }
